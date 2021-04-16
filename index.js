7d23fa4a2e9de37dadf47190ff3d315711434ee0
Shahed70 <58668503+Shahed70@users.noreply.github.com>
2021-04-17 04:51:12 +0600

    first server commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fileUpload = require('express-fileupload')
 const port = process.env.PORT || 4000
 require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient;
+const { ObjectID } = require('bson')
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.em8kw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -15,17 +16,38 @@ const reqMiddleware = [exprss.json(), urlencoded({extended:true}), cors(), exprs
 app.use(reqMiddleware)
 
 
-
-
-
-
 app.get('/', (req, res) => {
     res.send('Connected')
 })
 
 client.connect(err => {
-    const appointmentCollection = client.db("doctors-portal").collection("appointment");
-    console.log(uri);
+    const serviceCollection = client.db("laundryShop").collection("services");
+      app.post('/addService', (req, res) => {
+        const services = req.body;
+        serviceCollection.insertOne(services)
+        .then(result => {
+          res.send(result.insertedCount > 0);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+      })
+
+      app.get('/getServices', (req, res) =>{
+        serviceCollection.find({})
+        .toArray((err, docs) => {
+          res.send(docs)
+        })
+      })
+
+      app.get('/getServices/:id', (req, res) =>{
+        console.log(req.params.id);
+        serviceCollection.find({_id:ObjectID(req.params.id)})
+        .toArray((err, docs) => {
+          res.send(docs)
+         console.log(docs);
+        })
+      })
   });
 app.listen(port, () => console.log('App is listening on ' + port))
 
